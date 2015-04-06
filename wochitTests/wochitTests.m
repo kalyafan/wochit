97b6d3a180d4908e266e5e2df16352d2d0471a0b
mac <WardeN@Smartexe-iMac.local>
2015-04-06 13:41:41 +0600

    Initial Commit

---
+++ wochitTests/wochitTests.m
@@ -0,0 +1,40 @@
+//
+//  wochitTests.m
+//  wochitTests
+//
+//  Created by mac on 4/6/15.
+//  Copyright (c) 2015 smartexe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface wochitTests : XCTestCase
+
+@end
+
+@implementation wochitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
